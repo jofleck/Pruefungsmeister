638024598be937ec7a87a62a5ae0c92767f338e9
Jonas Fleck <jonas@flecknet.de>
2017-05-23 20:03:29 +0200

    Add description for resources

--- client/src/main/java/de/fhdortmund/swt2/pruefungsmeister/Controller/Resource.java
+++ client/src/main/java/de/fhdortmund/swt2/pruefungsmeister/Controller/Resource.java
@@ -4,12 +4,19 @@ package de.fhdortmund.swt2.pruefungsmeister.Controller;
  * Created by jonas on 22.05.17.
  */
 public enum Resource {
-    ENERGYDRINK(0), FASTFOOD(1), EXTRA_POINTS(2), KNOW_HOW(3), TECHNOLOGY(4);
+    ENERGYDRINK("Energydrink"), FASTFOOD("Fastfood"), EXTRA_POINTS("Bonuspunkte"), KNOW_HOW("Lernstoff"),
+    TECHNOLOGY("Technik");
 
-    private final int number;
-    Resource(int number) {
-        this.number = number;
+    private final String description;
+
+    Resource(String description) {
+        this.description = description;
     }
 
-    public int getIntValue() { return number; }
+    @Override
+    public String toString() {
+        return description;
     }
+}
+
+
